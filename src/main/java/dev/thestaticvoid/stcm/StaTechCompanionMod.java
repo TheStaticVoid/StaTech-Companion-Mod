0f1a9fd55f0e3fb6ef9f779bef0d31bb76e573f5
fmbellomy <fmbellomy@quantumgarbage.com>
2026-07-30 16:38:10 -0500

    not even sure why this was formatted incorrectly before

--- src/main/java/dev/thestaticvoid/stcm/StaTechCompanionMod.java
+++ src/main/java/dev/thestaticvoid/stcm/StaTechCompanionMod.java
@@ -3,34 +3,16 @@ package dev.thestaticvoid.stcm;
 import com.mojang.logging.LogUtils;
 import dev.thestaticvoid.stcm.guideme.StaTechGuide;
 import dev.thestaticvoid.stcm.space.SpaceItems;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
 
@@ -52,8 +34,7 @@ public class StaTechCompanionMod {
         SpaceItems.init();
         StaTechGuide.init();
 
-        modContainer.registerConfig(ModConfig.Type.COMMON, StaTechCompanionConfig.CONFIG_SPEC
-        );
+        modContainer.registerConfig(ModConfig.Type.COMMON, StaTechCompanionConfig.CONFIG_SPEC);
 
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
